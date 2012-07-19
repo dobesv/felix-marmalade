75411bb935a368450d50b2be9f777efdbad29e28
Dobes Vandermeer <dobesv@gmail.com>
2012-07-18 20:56:33 -0700

    Got an example running with a ninja who paces back and forth across the dojo.

--- flx.cmd
+++ flx.cmd
@@ -3,5 +3,5 @@ set MYDIR=%~dp0%
 @rem call "%VS100COMNTOOLS%vsvars32.bat"
 set PATH=%PATH%;%MYDIR%felix-dist\bin
 set FLX_INSTALL_DIR=%MYDIR%felix-dist
-flx --test=%FLX_INSTALL_DIR% -I%MYDIR%\src --bundle_dir=%PWD%\gen --nocc %*
+flx --test=%FLX_INSTALL_DIR% -I%MYDIR%\src -I%CD%\src --bundle_dir=%PWD%\gen --nocc %*
 
